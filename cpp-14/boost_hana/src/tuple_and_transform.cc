7b06db5b23aac6a638c9e7f493eb2159d9fe0028
edansam <sam.a.daniel@ericsson.com>
2017-03-10 08:38:25 +0100

    initial

---
+++ cpp-14/boost_hana/src/tuple_and_transform.cc
@@ -0,0 +1,26 @@
+#include <boost/hana.hpp>
+#include <cassert>
+#include <iostream>
+#include <string>
+
+namespace hana = boost::hana;
+
+struct fish { std::string name; };
+struct cat  { std::string name; };
+struct dog  { std::string name; };
+
+int main()
+{
+   
+   auto animals = hana::make_tuple(fish{"nemo"},cat{"tom"},dog{"scooby"});
+   
+   // Lets apply transform
+   auto animal_names = hana::transform(animals, [](/*generic lambda c++14*/auto obj)
+         {
+            return obj.name;
+         });
+
+   assert(animal_names == hana::make_tuple("nemo","tom","scooby");
+
+}
+
